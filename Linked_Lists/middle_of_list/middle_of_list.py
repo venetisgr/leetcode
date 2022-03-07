5154aa85b0303912eeaef6711af079de914d8077
venetisgr <38510475+venetisgr@users.noreply.github.com>
2022-03-07 13:30:03 -0500

    Add files via upload

---
+++ Linked_Lists/middle_of_list/middle_of_list.py
@@ -0,0 +1,15 @@
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        #Fast and Slow pointer
+        #Fst pointer does two steps while slow one. By the time fast reaches the end, slow is in the middle
+        
+        slow = head
+        fast = head
+        
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            
+        return slow
+        
